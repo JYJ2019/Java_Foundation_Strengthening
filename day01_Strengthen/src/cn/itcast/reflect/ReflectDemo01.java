3d36109a7cd8d1bec204f6ccde4e9b97c520a1f7
jyj <1036470143@qq.com>
2020-12-15 18:16:54 +0800

    基础加强

--- day01_Strengthen/src/cn/itcast/reflect/ReflectDemo01.java
+++ day01_Strengthen/src/cn/itcast/reflect/ReflectDemo01.java
@@ -1,5 +1,7 @@
 package cn.itcast.reflect;
 
+import cn.itcast.domain.Person;
+
 /**
  * @Description：获取Class对象的方式
  * @Author 金宇佳
@@ -13,5 +15,20 @@ package cn.itcast.reflect;
     3.对象.getClass(): getClass()方法在 Object类中定义着
  */
 public class ReflectDemo01 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        //1.Class.forName("全类名")
+        Class cls1 = Class.forName("cn.itcast.domain.Person");
+        System.out.println(cls1);
+        //2.类名.class
+        Class cls2 = Person.class;
+        System.out.println(cls2);
+        //3.对象.getClass()
+        Person p = new Person();
+        Class cls3 = p.getClass();
+        System.out.println(cls3);
 
+        //== 比较三个对象
+        System.out.println(cls1 == cls2);//true
+        System.out.println(cls1 == cls3);//true
+    }
 }
