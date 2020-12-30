c2c7c7da0b7f910116f9302a14afb86c53cc4155
jyj <1036470143@qq.com>
2020-12-30 11:20:18 +0800

    基础加强

--- day04_jdbc/src/cn/itcast/jdbc/JDBCDome7.java
+++ day04_jdbc/src/cn/itcast/jdbc/JDBCDome7.java
@@ -14,12 +14,21 @@ public class JDBCDome7 {
         Statement stmt = null;
         ResultSet resultSet = null;
         try {
+            //1.注册驱动
             Class.forName("com.mysql.jdbc.Driver");
+            //2.获取连接对象
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db3", "root", "root");
+            //3.定义sql
             String sql = "SELECT * FROM account";
+            //4.获取执行sql对象
             stmt = conn.createStatement();
+            //5.执行sql
             resultSet = stmt.executeQuery(sql);
+            //6.处理结果
+            //循环判断游标是否是最后一行末尾。
             while (resultSet.next()) {
+                //获取数据
+                //6.2获取数据
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("NAME");
                 String balance = resultSet.getString("balance");
