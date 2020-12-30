5bda4f50495d169d9f45170bdb8088527d48e121
jyj <1036470143@qq.com>
2020-12-30 19:51:07 +0800

    基础加强

--- day05_dataSource_jdbcTemplate/src/cn/itcast/datasource/druid/DruidDemo.java
+++ day05_dataSource_jdbcTemplate/src/cn/itcast/datasource/druid/DruidDemo.java
@@ -1,5 +1,13 @@
 package cn.itcast.datasource.druid;
 
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
 /**
  * @Description：Druid演示
  * @Author 金宇佳
@@ -7,7 +15,16 @@ package cn.itcast.datasource.druid;
  * @Version 1.0
  */
 public class DruidDemo {
-    //1.导入jar包
-    //2.定义配置文件
-    //3.加载配置文件
+    public static void main(String[] args) throws Exception {
+        //1.导入jar包
+        //2.定义配置文件
+        //3.加载配置文件
+        Properties pro = new Properties();
+        InputStream is = DruidDemo.class.getClassLoader().getResourceAsStream("druid.properties");
+        pro.load(is);
+        DataSource ds = DruidDataSourceFactory.createDataSource(pro);
+        Connection conn = ds.getConnection();
+        System.out.println(conn);
+    }
+
 }
