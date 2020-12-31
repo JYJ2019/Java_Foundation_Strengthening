088dc1b9a0e9c780bf6eb5be93ddc2f8717f1ec2
jyj <1036470143@qq.com>
2020-12-31 11:04:09 +0800

    基础加强

---
+++ day05_dataSource_jdbcTemplate/src/cn/itcast/datasource/druid/DruidDemo2.java
@@ -0,0 +1,42 @@
+package cn.itcast.datasource.druid;
+
+import cn.itcast.datasource.utils.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @Description：使用新的工具类
+ * @Author 金宇佳
+ * @Date 2020/12/30 19:55
+ * @Version 1.0
+ */
+public class DruidDemo2 {
+    public static void main(String[] args) {
+        /*
+            完成添加操作：给account表添加一条记录
+         */
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            //1.获取连接
+            conn = JDBCUtils.getConnection();
+            //2.定义sql
+            String sql = "INSERT INTO account(id,`NAME`,balance) VALUES (NULL,?,?)";
+            //3.获取pstmt对象
+            pstmt = conn.prepareStatement(sql);
+            //4.给？赋值
+            pstmt.setString(1, "王五");
+            pstmt.setDouble(2, 3000);
+            //5.执行sql
+            int count = pstmt.executeUpdate();
+            System.out.println(count);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            //6. 释放资源
+            JDBCUtils.close(pstmt, conn);
+        }
+    }
+}
---
+++ day05_dataSource_jdbcTemplate/src/cn/itcast/jdbctemplate/jdbcTemplateDemo1.java
@@ -0,0 +1,13 @@
+package cn.itcast.jdbctemplate;
+
+/**
+ * @Description：JdbcTemplate入门
+ * @Author 金宇佳
+ * @Date 2020/12/31 11:03
+ * @Version 1.0
+ */
+public class jdbcTemplateDemo1 {
+    public static void main(String[] args) {
+
+    }
+}
