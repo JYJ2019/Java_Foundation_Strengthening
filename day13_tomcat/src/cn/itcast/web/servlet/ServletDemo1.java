790bb38ee9d4b1b8fb0054ef555f48e9af42d69a
jyj <1036470143@qq.com>
2021-01-08 09:33:43 +0800

    基础加强

---
+++ day13_tomcat/src/cn/itcast/web/servlet/ServletDemo1.java
@@ -0,0 +1,44 @@
+package cn.itcast.web.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @Description：Servlet快速入门
+ * @Author 金宇佳
+ * @Date 2021/1/6 16:27
+ * @Version 1.0
+ */
+public class ServletDemo1 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    /**
+     * 提供服务的方法
+     * @param servletRequest
+     * @param servletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Hello Servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ day13_tomcat/src/cn/itcast/web/servlet/ServletDemo2.java
@@ -0,0 +1,65 @@
+package cn.itcast.web.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @Description：Servlet_生命周期方法
+ * @Author 金宇佳
+ * @Date 2021/1/6 17:14
+ * @Version 1.0
+ */
+public class ServletDemo2 implements Servlet {
+    /**
+     * 初始化方法
+     * 在Servlet 被创建时,执行.只会执行一次.
+     * @param servletConfig
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init.....");
+    }
+
+    /**
+     * 获取ServletConfig对象
+     * ServletConfig: Servlet的配置对象
+     * @return
+     */
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+
+    /**
+     * 提供服务方法
+     * 每一次Servlet被访问时,执行.执行多次
+     * @param servletRequest
+     * @param servletResponse
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service......");
+    }
+
+    /**
+     * 获取Servlet的一些信息,版本,作者等等...
+     * @return
+     */
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    /**
+     * 销毁方法
+     * 在服务器正常关闭时,执行,执行一次
+     */
+    @Override
+    public void destroy() {
+        System.out.println("destroy.....");
+    }
+}
