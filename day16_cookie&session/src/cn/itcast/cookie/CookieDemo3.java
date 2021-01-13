27a758cb69b28f68e3bf4cde849ca895e8c215ff
jyj <1036470143@qq.com>
2021-01-13 09:48:56 +0800

    基础加强

---
+++ day16_cookie&session/src/cn/itcast/cookie/CookieDemo3.java
@@ -0,0 +1,31 @@
+package cn.itcast.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description：Cookie快速入门
+ * @Author JYJ
+ * @Date 2021/1/12 9:19
+ * @Version 1.0
+ */
+@WebServlet("/cookieDemo3")
+public class CookieDemo3 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //1.创建Cookie对象
+        Cookie c1 = new Cookie("msg", "hello");
+        Cookie c2 = new Cookie("name", "zhangsan");
+        //2.发送Cookie
+        response.addCookie(c1);
+        response.addCookie(c2);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
